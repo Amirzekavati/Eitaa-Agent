a64ab3c95bd6b39247851d3297b9dc158aef0f54
amirz <amir.zekavati.a@gmail.com>
2024-07-22 04:47:12 +0330

    find the text of the message

--- Agent/agent.py
+++ Agent/agent.py
@@ -14,5 +14,6 @@ while True:
     soup = BeautifulSoup(response.content, "html.parser")
 
     message_title = soup.find(class_="etme_widget_message_owner_name").find("span").get_text(strip=True)
+    message_text = soup.find(class_="etme_widget_message_text js-message_text")
 
 
