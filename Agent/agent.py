302a5b8f634667ca272b72b33fa451363a7f1d39
amirz <amir.zekavati.a@gmail.com>
2024-07-22 04:41:48 +0330

    do HTML parse

--- Agent/agent.py
+++ Agent/agent.py
@@ -4,3 +4,12 @@ from bs4 import BeautifulSoup
 base_url = "https://eitta.com/s/akhbarefori/"
 message_number = 1
 messages = []
+
+while True:
+    url = f"{base_url}{message_number}"
+    response = requests.get(url)
+    if response.status_code != 200 :
+        break
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
