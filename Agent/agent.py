deac2e21bfae9f39cd8e5a4e26b61dd36971c622
amirz <amir.zekavati.a@gmail.com>
2024-07-22 05:27:34 +0330

    Extract the message_title in another way

--- Agent/agent.py
+++ Agent/agent.py
@@ -13,26 +13,19 @@ while True:
 
     soup = BeautifulSoup(response.content, "html.parser")
 
-    message_title = soup.find(class_="etme_widget_message_owner_name").find("span").get_text(strip=True)
-    message_text = soup.find(class_="etme_widget_message_text js-message_text")
-    message_view = soup.find(class_="etme_widget_message_info short js-message_info").find("span").get_text(strip=True)
-    message_time = soup.find(class_="time")
-    message_date = soup.find("a", class_="etme_widget_message_date").find("time").get("datetime")
+    # Initialize variables to None
+    message_title = None
+    message_text = None
+    message_view = None
+    message_time = None
+    message_date = None
 
-
-    message = {
-        "message_number": message_number,
-        "message_title": message_title,
-        "message_text": message_text,
-        "message_view": message_view,
-        "message_time": message_time,
-        "message_date": message_date
-    }
-    messages.append(message)
-    print(f"scraped message {message_number}")
-
-    message_number += 1
-
-    for message in messages:
-        print(message)
+    # Extract message title
+    author_element = soup.find("div", class_="etme_widget_message_author accent_color")
+    if author_element:
+        owner_name_element = author_element.find("a", class_="etme_widget_message_owner_name")
+        if owner_name_element:
+            span_element = owner_name_element.find("span", dir="auto")
+            if span_element:
+                message_title = span_element.get_text(strip=True)
 
