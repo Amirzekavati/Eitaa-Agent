c5d676d5bdd62f7ad8093fccf7f75fba196f7388
amirz <amir.zekavati.a@gmail.com>
2024-07-22 04:54:40 +0330

    add dictionary to array

--- Agent/agent.py
+++ Agent/agent.py
@@ -15,5 +15,20 @@ while True:
 
     message_title = soup.find(class_="etme_widget_message_owner_name").find("span").get_text(strip=True)
     message_text = soup.find(class_="etme_widget_message_text js-message_text")
+    message_view = soup.find(class_="etme_widget_message_info short js-message_info").find("span").get_text(strip=True)
+    message_time = soup.find(class_="time")
+    message_date = soup.find("a", class_="etme_widget_message_date").find("time").get("datetime")
+
+
+    message = {
+        "message_number": message_number,
+        "message_title": message_title,
+        "message_text": message_text,
+        "message_view": message_view,
+        "message_time": message_time,
+        "message_date": message_date
+    }
+    messages.append(message)
+    print(f"scraped message {message_number}")
 
 
