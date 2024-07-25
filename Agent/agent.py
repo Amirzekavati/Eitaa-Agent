e7b66de70f074a2cb8afcf483b9bc7f905adc3a1
amirz <amir.zekavati.a@gmail.com>
2024-07-25 15:12:42 +0330

    add username and name for group or channel

--- Agent/agent.py
+++ Agent/agent.py
@@ -6,17 +6,17 @@ from database import AgentDataBase
 class EitaaAgent:
     def __int__(self):
         self.db = AgentDataBase()
+        self.message_number = 0
 
-    def crawl_and_insert_into_database(self, url, start_date, end_date):
-        message_number = 0
+    def crawl_and_insert_specific_date(self, url, start_date, end_date):
 
         start_date = datetime.strptime(start_date, "%Y-%m-%dT%H:%M:%S%z").date()
         end_date = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S%z").date() + timedelta(
             days=1)  # Inclusive of end date
 
         while True:
-            message_number += 1
-            url = f"{url}{message_number}"
+            self.message_number += 1
+            url = f"{url}{self.message_number}"
             response = requests.get(url)
             if response.status_code != 200:
                 break
@@ -39,6 +39,10 @@ class EitaaAgent:
             message_date = soup.find("time", class_="time").get("datetime")
             # Extract message id
             message_id = soup.find("div", class_="etme_widget_message_wrap js-widget_message_wrap").get("id")
+            # Extract the group or channel name
+            name = soup.find("div", class_="etme_channel_info_header_title").find("span", dir="auto").get_text(strip=True)
+            # Extract the username
+            username = soup.find("div", class_="etme_channel_info_header_username").find("a", dir="auto").get_text()
 
             message = {
                 "id": message_id,
@@ -46,72 +50,11 @@ class EitaaAgent:
                 "text": message_text,
                 "view": message_view,
                 "time": message_time,
-                "date": message_date
+                "date": message_date,
+                "name": name,
+                "username": username
             }
 
-            message_date = datetime.strptime(message_date, "%Y-%m-%dT%H:%M:%S%z")
+            message_date = datetime.strptime(message_date, "%Y-%m-%dT%H:%M:%S%z").date()
             if start_date <= message_date <= end_date:
                 self.db.upsert(message)
-
-
-
-
-# import requests
-# from bs4 import BeautifulSoup
-#
-# base_url = "https://eitaa.com/akhbarefori/"
-# message_number = 0
-# messages = []
-#
-# while True:
-#     message_number += 1
-#     url = f"{base_url}{message_number}"
-#     response = requests.get(url)
-#     if response.status_code != 200:
-#         break
-#
-#     soup = BeautifulSoup(response.content, "html.parser")
-#
-#     # Initialize variables to None
-#     message_title = None
-#     message_text = None
-#     message_view = None
-#     message_time = None
-#     message_date = None
-#     message_id = None
-#
-#     # Extract message title
-#     author_element = soup.find("div", class_="etme_widget_message_author accent_color")
-#     if author_element:
-#         owner_name_element = author_element.find("a", class_="etme_widget_message_owner_name")
-#         if owner_name_element:
-#             span_element = owner_name_element.find("span", dir="auto")
-#             if span_element:
-#                 message_title = span_element.get_text(strip=True)
-#
-#     # Extract message text
-#     message_text = soup.find("div", class_="etme_widget_message_text js-message_text").get_text()
-#     # Extract message view
-#     message_view = soup.find("span", class_="etme_widget_message_views").get_text()
-#     # Extract message time
-#     message_time = soup.find("time", class_="time").get_text()
-#     # Extract message date
-#     message_date = soup.find("time", class_="time").get("datetime")
-#     # Extract message id
-#     message_id = soup.find("div", class_="etme_widget_message_wrap js-widget_message_wrap").get("id")
-#
-#     message={
-#         "id": message_id,
-#         "title": message_title,
-#         "text": message_text,
-#         "view": message_view,
-#         "time": message_time,
-#         "date": message_date
-#     }
-#
-#     print("Message Id:", message_id)
-#     print("Message Title:", message_title)
-#     print("Message Text:", message_text)
-#     print("Message View:", message_view)
-#     print("Message Time:", message_time)
-#     print("Message Date:", message_date)
